b106abe69738c7cf54908b0ef65eb9494f4702fe
Mason Remaley <mason@anthropicstudios.com>
2025-05-23 16:59:03 -0700

    Adds typed writer

--- src/Writer.zig
+++ src/Writer.zig
@@ -7,8 +7,36 @@ const Self = @This();
 const Dest = [*]volatile u8;
 
 ptr: *volatile anyopaque,
-pos: u64 = 0,
-size: u64 = 0,
+pos: u64,
+size: u64,
+
+/// Similar to `Writer`, but can only write type `T`.
+pub fn Typed(T: type) type {
+    return struct {
+        ptr: *volatile anyopaque,
+        capacity: u64,
+        len: u64,
+
+        pub fn write(self: *@This(), value: T) void {
+            comptime assert(@typeInfo(T).@"struct".layout != .auto);
+            const offset = std.math.mul(u64, self.len, @sizeOf(T)) catch @panic("OOB");
+            if (offset >= self.capacity * @sizeOf(T)) @panic("OOB");
+            const dest: *align(1) volatile T = @ptrFromInt(@intFromPtr(self.ptr) + offset);
+            dest.* = value;
+            self.len += 1;
+        }
+    };
+}
+
+/// Returns a typed writer of the given type. Asserts that writer is properly aligned for the type.
+pub fn typed(self: @This(), T: type) Typed(T) {
+    assert(self.pos % @alignOf(T) == 0);
+    return .{
+        .ptr = @ptrFromInt(@intFromPtr(self.ptr) + self.pos),
+        .len = 0,
+        .capacity = (self.size - self.pos) / @sizeOf(T),
+    };
+}
 
 /// Writes the given bytes to the writer, panicking if it goes out of bounds.
 pub inline fn writeAll(self: *Self, bytes: []const u8) void {
@@ -40,7 +68,6 @@ pub inline fn writeStructAligned(self: *Self, value: anytype) void {
 ///
 /// https://docs.vulkan.org/spec/latest/chapters/fundamentals.html#fundamentals-host-environment
 pub inline fn writeStructUnaligned(self: *Self, value: anytype) void {
-    // Implementation manually inlined to improve debug mode performance
     comptime assert(@typeInfo(@TypeOf(value)).@"struct".layout != .auto);
     const new_pos = std.math.add(u64, self.pos, @sizeOf(@TypeOf(value))) catch @panic("OOB");
     if (new_pos > self.size) @panic("OOB");
