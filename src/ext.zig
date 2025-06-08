4979accb0a8679fae9ebeb39fe69d5fb77d4199d
Mason Remaley <mason@anthropicstudios.com>
2025-06-08 15:20:05 -0700

    Renames timer

--- src/ext.zig
+++ src/ext.zig
@@ -5,6 +5,6 @@ const image_bump_allocator = @import("ext/image_bump_allocator.zig");
 pub const ImageBumpAllocator = @import("ext/image_bump_allocator.zig").ImageBumpAllocator;
 pub const DeleteQueue = @import("ext/delete_queue.zig").DeleteQueue;
 pub const ImageUploadQueue = @import("ext/ImageUploadQueue.zig");
-pub const ShaderTimer = @import("ext/ShaderTimer.zig");
+pub const ModTimer = @import("ext/mod_timer.zig").ModTimer;
 pub const bufPart = @import("ext/buf_part.zig").bufPart;
 pub const colors = @import("ext/colors.zig");
---
+++ src/ext/mod_timer.zig
@@ -0,0 +1,22 @@
+//! See `ModTimer`.
+
+/// A looping timer. Useful for shader effects that need time as an input, but don't want to run out
+/// of precision when the game has been running for a while.
+pub const ModTimer = extern struct {
+    /// The frequency of the timer.
+    ///
+    /// Defaulting to 1000 allows periodic periodic effects to have up to three decimals of precision in
+    /// their frequency when measured in seconds without causing a hitch when the timer resets.
+    frequency: f32 = 1000,
+
+    /// The current value of the timer in seconds.
+    ///
+    /// By initializing to a value near the point where the timer wraps, we're more likely to catch
+    /// effects that don't correctly handle the wrap.
+    seconds: f32 = 1000 - 10,
+
+    /// Update the timer.
+    pub fn update(self: *@This(), delta_s: f32) void {
+        self.seconds = @rem(self.seconds + delta_s, self.frequency);
+    }
+};
