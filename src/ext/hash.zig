248a9ce228d6b410dd91cbfb9109c5793dad73ff
Mason Remaley <mason@anthropicstudios.com>
2025-06-14 00:46:05 -0700

    Adds rand to hash extension

--- src/ext/hash.zig
+++ src/ext/hash.zig
@@ -1,10 +1,10 @@
-//! Mirrors of the hash functions provided by GBMS, see GBMS for documentation, origins, recommended
-//! usage, etc:
+//! Mirrors of the hash and and hash based rand functions provided by GBMS, see GBMS for
+//! documentation, origins, recommended usage, etc:
 //!
 //! https://github.com/Games-by-Mason/gbms/
 //!
 //! Definitely not cryptographically secure. Probably not good in hash maps either. Tuned for
-//! graphics.
+//! graphics, prefer the Zig standard library for other needs.
 
 const std = @import("std");
 
@@ -14,9 +14,6 @@ fn pcg(s: u32) u32 {
     return (word >> 22) ^ word;
 }
 
-// XXX: actually test these visually? with noise maybe?
-// XXX: add tests just to make sure that they compile at least
-// XXX: maybe also check soem random values for overflow
 test pcg {
     // Check single value
     try std.testing.expectEqual(129708002, pcg(0));
@@ -110,14 +107,13 @@ test pcg4d {
     }
 }
 
-// XXX: test these
-fn default1D(s: u32) u32 {
+fn default(s: u32) u32 {
     return pcg(s);
 }
 
-test default1D {
+test default {
     // Make sure it compiles
-    _ = default1D(0);
+    _ = default(0);
 }
 
 fn default2D(s: @Vector(2, u32)) @Vector(2, u32) {
@@ -129,7 +125,7 @@ test default2D {
     _ = default2D(.{ 0, 1 });
 }
 
-fn default3D(s: [3]u32) [3]u32 {
+fn default3D(s: @Vector(3, u32)) @Vector(3, u32) {
     return pcg3d(s);
 }
 
@@ -145,4 +141,156 @@ fn default4D(s: @Vector(4, u32)) @Vector(4, u32) {
 test default4D {
     // Make sure it compiles
     _ = default4D(.{ 0, 1, 2, 3 });
+}
+
+fn rand(s: anytype) f32 {
+    switch (@TypeOf(s)) {
+        u32 => {
+            const all: f32 = @floatFromInt(default(s));
+            return all / std.math.maxInt(u32);
+        },
+        @Vector(2, u32) => {
+            const all: @Vector(2, f32) = @floatFromInt(default2D(s));
+            return all[0] / std.math.maxInt(u32);
+        },
+        @Vector(3, u32) => {
+            const all: @Vector(3, f32) = @floatFromInt(default3D(s));
+            return all[0] / std.math.maxInt(u32);
+        },
+        @Vector(4, u32) => {
+            const all: @Vector(4, f32) = @floatFromInt(default4D(s));
+            return all[0] / std.math.maxInt(u32);
+        },
+        f32 => return rand(@as(u32, @bitCast(s))),
+        @Vector(2, f32) => return rand(@as(@Vector(2, u32), @bitCast(s))),
+        @Vector(3, f32) => return rand(@as(@Vector(3, u32), @bitCast(s))),
+        @Vector(4, f32) => return rand(@as(@Vector(4, u32), @bitCast(s))),
+        else => comptime unreachable,
+    }
+}
+
+test rand {
+    // Make sure it compiles for each type
+    try std.testing.expectEqual(6.591631e-1, rand(@as(u32, 1)));
+    try std.testing.expectEqual(1.0669651e-2, rand(@Vector(2, u32){ 1, 2 }));
+    try std.testing.expectEqual(9.789959e-1, rand(@Vector(3, u32){ 1, 2, 3 }));
+    try std.testing.expectEqual(2.1146852e-1, rand(@Vector(4, u32){ 1, 2, 3, 4 }));
+    try std.testing.expectEqual(9.4426405e-1, rand(@as(f32, 1)));
+    try std.testing.expectEqual(3.514351e-1, rand(@Vector(2, f32){ 1, 2 }));
+    try std.testing.expectEqual(6.142317e-1, rand(@Vector(3, f32){ 1, 2, 3 }));
+    try std.testing.expectEqual(9.8791474e-1, rand(@Vector(4, f32){ 1, 2, 3, 4 }));
+}
+
+fn rand2(s: anytype) @Vector(2, f32) {
+    switch (@TypeOf(s)) {
+        u32 => {
+            const all: @Vector(2, f32) = @floatFromInt(default2D(.{ s, 1 }));
+            return all / @as(@Vector(2, f32), @splat(std.math.maxInt(u32)));
+        },
+        @Vector(2, u32) => {
+            const all: @Vector(2, f32) = @floatFromInt(default2D(s));
+            return all / @as(@Vector(2, f32), @splat(std.math.maxInt(u32)));
+        },
+        @Vector(3, u32) => {
+            const all: @Vector(3, f32) = @floatFromInt(default3D(s));
+            return @Vector(2, f32){ all[0], all[1] } / @as(@Vector(2, f32), @splat(std.math.maxInt(u32)));
+        },
+        @Vector(4, u32) => {
+            const all: @Vector(4, f32) = @floatFromInt(default4D(s));
+            return @Vector(2, f32){ all[0], all[1] } / @as(@Vector(2, f32), @splat(std.math.maxInt(u32)));
+        },
+        f32 => return rand2(@as(u32, @bitCast(s))),
+        @Vector(2, f32) => return rand2(@as(@Vector(2, u32), @bitCast(s))),
+        @Vector(3, f32) => return rand2(@as(@Vector(3, u32), @bitCast(s))),
+        @Vector(4, f32) => return rand2(@as(@Vector(4, u32), @bitCast(s))),
+        else => comptime unreachable,
+    }
+}
+
+test rand2 {
+    // Make sure it compiles for each type
+    try std.testing.expectEqual(@Vector(2, f32){ 3.076969e-1, 9.435365e-1 }, rand2(@as(u32, 1)));
+    try std.testing.expectEqual(@Vector(2, f32){ 1.0669651e-2, 4.9842097e-2 }, rand2(@Vector(2, u32){ 1, 2 }));
+    try std.testing.expectEqual(@Vector(2, f32){ 9.789959e-1, 2.849572e-1 }, rand2(@Vector(3, u32){ 1, 2, 3 }));
+    try std.testing.expectEqual(@Vector(2, f32){ 2.1146852e-1, 9.417182e-1 }, rand2(@Vector(4, u32){ 1, 2, 3, 4 }));
+    try std.testing.expectEqual(@Vector(2, f32){ 9.4483894e-1, 6.1725557e-1 }, rand2(@as(f32, 1)));
+    try std.testing.expectEqual(@Vector(2, f32){ 3.514351e-1, 5.474391e-1 }, rand2(@Vector(2, f32){ 1, 2 }));
+    try std.testing.expectEqual(@Vector(2, f32){ 6.142317e-1, 5.6874275e-1 }, rand2(@Vector(3, f32){ 1, 2, 3 }));
+    try std.testing.expectEqual(@Vector(2, f32){ 9.8791474e-1, 4.3170738e-1 }, rand2(@Vector(4, f32){ 1, 2, 3, 4 }));
+}
+
+fn rand3(s: anytype) @Vector(3, f32) {
+    switch (@TypeOf(s)) {
+        u32 => {
+            const all: @Vector(3, f32) = @floatFromInt(default3D(.{ s, 1, 1 }));
+            return all / @as(@Vector(3, f32), @splat(std.math.maxInt(u32)));
+        },
+        @Vector(2, u32) => {
+            const all: @Vector(3, f32) = @floatFromInt(default3D(.{ s[0], s[1], 1 }));
+            return all / @as(@Vector(3, f32), @splat(std.math.maxInt(u32)));
+        },
+        @Vector(3, u32) => {
+            const all: @Vector(3, f32) = @floatFromInt(default3D(s));
+            return all / @as(@Vector(3, f32), @splat(std.math.maxInt(u32)));
+        },
+        @Vector(4, u32) => {
+            const all: @Vector(4, f32) = @floatFromInt(default4D(s));
+            return @Vector(3, f32){ all[0], all[1], all[2] } / @as(@Vector(3, f32), @splat(std.math.maxInt(u32)));
+        },
+        f32 => return rand3(@as(u32, @bitCast(s))),
+        @Vector(2, f32) => return rand3(@as(@Vector(2, u32), @bitCast(s))),
+        @Vector(3, f32) => return rand3(@as(@Vector(3, u32), @bitCast(s))),
+        @Vector(4, f32) => return rand3(@as(@Vector(4, u32), @bitCast(s))),
+        else => comptime unreachable,
+    }
+}
+
+test rand3 {
+    // Make sure it compiles for each type
+    try std.testing.expectEqual(@Vector(3, f32){ 8.722949e-1, 5.462977e-1, 3.7529972e-1 }, rand3(@as(u32, 1)));
+    try std.testing.expectEqual(@Vector(3, f32){ 1.5867977e-1, 4.5829082e-1, 8.812844e-1 }, rand3(@Vector(2, u32){ 1, 2 }));
+    try std.testing.expectEqual(@Vector(3, f32){ 9.789959e-1, 2.849572e-1, 3.4935537e-1 }, rand3(@Vector(3, u32){ 1, 2, 3 }));
+    try std.testing.expectEqual(@Vector(3, f32){ 2.1146852e-1, 9.417182e-1, 8.791596e-1 }, rand3(@Vector(4, u32){ 1, 2, 3, 4 }));
+    try std.testing.expectEqual(@Vector(3, f32){ 9.415978e-1, 3.22427e-1, 7.810834e-1 }, rand3(@as(f32, 1)));
+    try std.testing.expectEqual(@Vector(3, f32){ 7.593601e-1, 5.267169e-1, 8.9228565e-1 }, rand3(@Vector(2, f32){ 1, 2 }));
+    try std.testing.expectEqual(@Vector(3, f32){ 6.142317e-1, 5.6874275e-1, 1.813631e-1 }, rand3(@Vector(3, f32){ 1, 2, 3 }));
+    try std.testing.expectEqual(@Vector(3, f32){ 9.8791474e-1, 4.3170738e-1, 6.5100867e-1 }, rand3(@Vector(4, f32){ 1, 2, 3, 4 }));
+}
+
+fn rand4(s: anytype) @Vector(4, f32) {
+    switch (@TypeOf(s)) {
+        u32 => {
+            const all: @Vector(4, f32) = @floatFromInt(default4D(.{ s, 1, 1, 1 }));
+            return all / @as(@Vector(4, f32), @splat(std.math.maxInt(u32)));
+        },
+        @Vector(2, u32) => {
+            const all: @Vector(4, f32) = @floatFromInt(default4D(.{ s[0], s[1], 1, 1 }));
+            return all / @as(@Vector(4, f32), @splat(std.math.maxInt(u32)));
+        },
+        @Vector(3, u32) => {
+            const all: @Vector(4, f32) = @floatFromInt(default4D(.{ s[0], s[1], s[2], 1 }));
+            return all / @as(@Vector(4, f32), @splat(std.math.maxInt(u32)));
+        },
+        @Vector(4, u32) => {
+            const all: @Vector(4, f32) = @floatFromInt(default4D(s));
+            return all / @as(@Vector(4, f32), @splat(std.math.maxInt(u32)));
+        },
+        f32 => return rand4(@as(u32, @bitCast(s))),
+        @Vector(2, f32) => return rand4(@as(@Vector(2, u32), @bitCast(s))),
+        @Vector(3, f32) => return rand4(@as(@Vector(3, u32), @bitCast(s))),
+        @Vector(4, f32) => return rand4(@as(@Vector(4, u32), @bitCast(s))),
+        else => comptime unreachable,
+    }
+}
+
+test rand4 {
+    // Make sure it compiles for each type
+    try std.testing.expectEqual(@Vector(4, f32){ 6.947346e-1, 4.1038275e-1, 9.989965e-1, 1.7831983e-1 }, rand4(@as(u32, 1)));
+    try std.testing.expectEqual(@Vector(4, f32){ 3.3835035e-2, 2.1328963e-1, 9.1972396e-2, 4.4690752e-1 }, rand4(@Vector(2, u32){ 1, 2 }));
+    try std.testing.expectEqual(@Vector(4, f32){ 2.0368083e-1, 8.241063e-1, 1.1822319e-1, 1.2990429e-2 }, rand4(@Vector(3, u32){ 1, 2, 3 }));
+    try std.testing.expectEqual(@Vector(4, f32){ 2.1146852e-1, 9.417182e-1, 8.791596e-1, 1.0639917e-2 }, rand4(@Vector(4, u32){ 1, 2, 3, 4 }));
+    try std.testing.expectEqual(@Vector(4, f32){ 2.6033977e-1, 1.3925232e-1, 2.4095826e-2, 2.6369214e-2 }, rand4(@as(f32, 1)));
+    try std.testing.expectEqual(@Vector(4, f32){ 5.175956e-1, 4.7401184e-1, 2.0530142e-1, 9.6941704e-1 }, rand4(@Vector(2, f32){ 1, 2 }));
+    try std.testing.expectEqual(@Vector(4, f32){ 8.658684e-1, 4.7979563e-1, 1.226957e-1, 6.556808e-1 }, rand4(@Vector(3, f32){ 1, 2, 3 }));
+    try std.testing.expectEqual(@Vector(4, f32){ 9.8791474e-1, 4.3170738e-1, 6.5100867e-1, 4.631154e-1 }, rand4(@Vector(4, f32){ 1, 2, 3, 4 }));
 }
