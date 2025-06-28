5bf7d118bc9cbced144137f344438d966044819a
Mason Remaley <mason@anthropicstudios.com>
2025-06-28 13:51:05 -0700

    Better render target pool namespacing

--- src/ext.zig
+++ src/ext.zig
@@ -7,8 +7,8 @@ const image_bump_allocator = @import("ext/image_bump_allocator.zig");
 pub const ImageBumpAllocator = @import("ext/image_bump_allocator.zig").ImageBumpAllocator;
 pub const DeleteQueue = @import("ext/delete_queue.zig").DeleteQueue;
 pub const ImageUploadQueue = @import("ext/ImageUploadQueue.zig");
-pub const RenderTargetPool = @import("ext/render_target_pool.zig").RenderTargetPool;
 pub const ModTimer = @import("ext/mod_timer.zig").ModTimer;
+pub const RenderTarget = @import("ext/render_target.zig").RenderTarget;
 pub const bufPart = @import("ext/buf_part.zig").bufPart;
 pub const colors = @import("ext/colors.zig");
 
