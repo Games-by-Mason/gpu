c6abe970e45c2faee111011494cc6b0868cef3ce
Mason Remaley <mason@anthropicstudios.com>
2025-08-07 21:35:09 -0700

    Removes bounded array from delete queue

--- src/ext.zig
+++ src/ext.zig
@@ -5,7 +5,7 @@ const std = @import("std");
 const image_bump_allocator = @import("ext/image_bump_allocator.zig");
 
 pub const ImageBumpAllocator = @import("ext/image_bump_allocator.zig").ImageBumpAllocator;
-pub const DeleteQueue = @import("ext/delete_queue.zig").DeleteQueue;
+pub const DeleteQueue = @import("ext/DeleteQueue.zig");
 pub const ImageUploadQueue = @import("ext/ImageUploadQueue.zig");
 pub const ModTimer = @import("ext/mod_timer.zig").ModTimer;
 pub const RenderTarget = @import("ext/render_target.zig").RenderTarget;
---
+++ src/ext/DeleteQueue.zig
@@ -0,0 +1,75 @@
+//! In modern graphics APIs, you often have to wait until a resource is no longer in use by the GPU
+//! to free it. Often this requires waiting until the frame in flight counter loops. This structure
+//! makes this a bit easier.
+//!
+//! Your renderer will typically store one delete queue per frame in flight, and reset it at the
+//! beginning of every frame. If you want to destroy a resource once the frame in flight loops,
+//! you can just enqueue it to this queue.
+
+const std = @import("std");
+const gpu = @import("../root.zig");
+
+const assert = std.debug.assert;
+
+const Gx = gpu.Gx;
+const Allocator = std.mem.Allocator;
+
+/// Represents a handle queued for destruction.
+pub const Handle = struct {
+    /// The integer value of the handle.
+    value: u64,
+    /// A pointer to the handle's delete function.
+    deinit: *const fn (value: u64, gx: *Gx) void,
+};
+
+/// The list of handles queued for deletion.
+handles: std.ArrayListUnmanaged(Handle) = .{},
+
+pub fn initCapacity(gpa: Allocator, capacity: usize) Allocator.Error!@This() {
+    return .{ .handles = try std.ArrayListUnmanaged(Handle).initCapacity(gpa, capacity) };
+}
+
+pub fn deinit(self: *@This(), gpa: Allocator, gx: *Gx) void {
+    self.reset(gx);
+    self.handles.deinit(gpa);
+}
+
+/// Appends the given resource to the delete queue.
+///
+/// Checks for various relevant fields on GPU types.
+pub fn append(self: *@This(), resource: anytype) void {
+    switch (@typeInfo(@TypeOf(resource))) {
+        .@"enum" => {
+            comptime assert(@sizeOf(@TypeOf(resource)) == @sizeOf(u64));
+            const Deinit = struct {
+                fn deinit(value: u64, gx: *Gx) void {
+                    const handle: @TypeOf(resource) = @enumFromInt(value);
+                    handle.deinit(gx);
+                }
+            };
+            if (self.handles.items.len >= self.handles.capacity) @panic("OOB");
+            self.handles.appendAssumeCapacity(.{
+                .value = @intFromEnum(resource),
+                .deinit = &Deinit.deinit,
+            });
+        },
+        .@"struct" => {
+            self.append(resource.handle);
+            if (@hasField(@TypeOf(resource), "view")) {
+                self.append(resource.view);
+            }
+            if (@hasField(@TypeOf(resource), "memory")) {
+                self.append(resource.memory);
+            }
+        },
+        else => comptime unreachable,
+    }
+}
+
+/// Frees all queues resources and resets the queue.
+pub fn reset(self: *@This(), gx: *Gx) void {
+    for (self.handles.items) |handle| {
+        handle.deinit(handle.value, gx);
+    }
+    self.handles.clearRetainingCapacity();
+}
